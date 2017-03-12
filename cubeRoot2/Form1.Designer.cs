253b4558ef382e671d2503bb80ec04f98978458f
HoutarouOreki <TheZjarek@gmail.com>
2017-03-13 00:07:04 +0100

    Initial commit

---
+++ cubeRoot2/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace cubeRoot2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timeLeftVar = new System.Windows.Forms.Label();
+            this.timeLeftText = new System.Windows.Forms.Label();
+            this.cubedLabel1 = new System.Windows.Forms.Label();
+            this.answer1 = new System.Windows.Forms.NumericUpDown();
+            this.answer2 = new System.Windows.Forms.NumericUpDown();
+            this.answer3 = new System.Windows.Forms.NumericUpDown();
+            this.answer4 = new System.Windows.Forms.NumericUpDown();
+            this.answer5 = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cubedLabel2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cubedLabel3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cubedLabel4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cubedLabel5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.levelText = new System.Windows.Forms.Label();
+            this.levelVar = new System.Windows.Forms.Label();
+            this.scoreVal = new System.Windows.Forms.Label();
+            this.scoreText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.answer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timeLeftVar
+            // 
+            this.timeLeftVar.BackColor = System.Drawing.SystemColors.ControlLight;
+            resources.ApplyResources(this.timeLeftVar, "timeLeftVar");
+            this.timeLeftVar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.timeLeftVar.Name = "timeLeftVar";
+            // 
+            // timeLeftText
+            // 
+            resources.ApplyResources(this.timeLeftText, "timeLeftText");
+            this.timeLeftText.Name = "timeLeftText";
+            // 
+            // cubedLabel1
+            // 
+            resources.ApplyResources(this.cubedLabel1, "cubedLabel1");
+            this.cubedLabel1.Name = "cubedLabel1";
+            // 
+            // answer1
+            // 
+            resources.ApplyResources(this.answer1, "answer1");
+            this.answer1.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.answer1.Name = "answer1";
+            this.answer1.ValueChanged += new System.EventHandler(this.answer1_ValueChanged);
+            this.answer1.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // answer2
+            // 
+            resources.ApplyResources(this.answer2, "answer2");
+            this.answer2.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.answer2.Name = "answer2";
+            this.answer2.ValueChanged += new System.EventHandler(this.answer2_ValueChanged);
+            this.answer2.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // answer3
+            // 
+            resources.ApplyResources(this.answer3, "answer3");
+            this.answer3.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.answer3.Name = "answer3";
+            this.answer3.ValueChanged += new System.EventHandler(this.answer3_ValueChanged);
+            this.answer3.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // answer4
+            // 
+            resources.ApplyResources(this.answer4, "answer4");
+            this.answer4.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.answer4.Name = "answer4";
+            this.answer4.ValueChanged += new System.EventHandler(this.answer4_ValueChanged);
+            this.answer4.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // answer5
+            // 
+            resources.ApplyResources(this.answer5, "answer5");
+            this.answer5.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.answer5.Name = "answer5";
+            this.answer5.ValueChanged += new System.EventHandler(this.answer5_ValueChanged);
+            this.answer5.Enter += new System.EventHandler(this.answer_Enter);
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // cubedLabel2
+            // 
+            resources.ApplyResources(this.cubedLabel2, "cubedLabel2");
+            this.cubedLabel2.Name = "cubedLabel2";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // cubedLabel3
+            // 
+            resources.ApplyResources(this.cubedLabel3, "cubedLabel3");
+            this.cubedLabel3.Name = "cubedLabel3";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // cubedLabel4
+            // 
+            resources.ApplyResources(this.cubedLabel4, "cubedLabel4");
+            this.cubedLabel4.Name = "cubedLabel4";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // cubedLabel5
+            // 
+            resources.ApplyResources(this.cubedLabel5, "cubedLabel5");
+            this.cubedLabel5.Name = "cubedLabel5";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // levelText
+            // 
+            resources.ApplyResources(this.levelText, "levelText");
+            this.levelText.Name = "levelText";
+            // 
+            // levelVar
+            // 
+            this.levelVar.BackColor = System.Drawing.SystemColors.ControlLight;
+            resources.ApplyResources(this.levelVar, "levelVar");
+            this.levelVar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.levelVar.Name = "levelVar";
+            // 
+            // scoreVal
+            // 
+            this.scoreVal.BackColor = System.Drawing.SystemColors.ControlLight;
+            resources.ApplyResources(this.scoreVal, "scoreVal");
+            this.scoreVal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.scoreVal.Name = "scoreVal";
+            // 
+            // scoreText
+            // 
+            resources.ApplyResources(this.scoreText, "scoreText");
+            this.scoreText.Name = "scoreText";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.scoreVal);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.levelVar);
+            this.Controls.Add(this.levelText);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cubedLabel5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cubedLabel4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cubedLabel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cubedLabel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.answer5);
+            this.Controls.Add(this.answer4);
+            this.Controls.Add(this.answer3);
+            this.Controls.Add(this.answer2);
+            this.Controls.Add(this.answer1);
+            this.Controls.Add(this.cubedLabel1);
+            this.Controls.Add(this.timeLeftText);
+            this.Controls.Add(this.timeLeftVar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.answer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.answer5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLeftVar;
+        private System.Windows.Forms.Label timeLeftText;
+        private System.Windows.Forms.Label cubedLabel1;
+        private System.Windows.Forms.NumericUpDown answer1;
+        private System.Windows.Forms.NumericUpDown answer2;
+        private System.Windows.Forms.NumericUpDown answer3;
+        private System.Windows.Forms.NumericUpDown answer4;
+        private System.Windows.Forms.NumericUpDown answer5;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cubedLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label cubedLabel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label cubedLabel4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label cubedLabel5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label levelText;
+        private System.Windows.Forms.Label levelVar;
+        private System.Windows.Forms.Label scoreVal;
+        private System.Windows.Forms.Label scoreText;
+    }
+}
+
---
+++ cubeRoot2/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cubeRoot2
+{
+    public partial class Form1 : Form
+    {
+        Random randomizer = new Random();
+        int cubed1, cubed2, cubed3, cubed4, cubed5, befCube1, befCube2, befCube3, befCube4, befCube5, level, fullTime, score;
+        double timeLeft;
+        bool firstTime = true;
+        bool afterFail = false;
+        
+        private void answer3_ValueChanged(object sender, EventArgs e)
+        {
+            if (befCube3 == answer3.Value)
+                answer3.Enabled = false;
+        }
+
+        private void answer4_ValueChanged(object sender, EventArgs e)
+        {
+            if (befCube4 == answer4.Value)
+                answer4.Enabled = false;
+        }
+
+        private void answer5_ValueChanged(object sender, EventArgs e)
+        {
+            if (befCube5 == answer5.Value)
+                answer5.Enabled = false;
+        }
+
+        private void answer2_ValueChanged(object sender, EventArgs e)
+        {
+            if (befCube2 == answer2.Value)
+                answer2.Enabled = false;
+        }
+
+        private void answer1_ValueChanged(object sender, EventArgs e)
+        {
+            if (befCube1 == answer1.Value)
+                answer1.Enabled = false;
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            befCube1 = randomizer.Next(11, 100);
+            Debug.WriteLine("\n1 = " + befCube1.ToString());
+            befCube2 = randomizer.Next(11, 100);
+            Debug.WriteLine("2 = " + befCube2.ToString());
+            befCube3 = randomizer.Next(11, 100);
+            Debug.WriteLine("3 = " + befCube3.ToString());
+            befCube4 = randomizer.Next(11, 100);
+            Debug.WriteLine("4 = " + befCube4.ToString());
+            befCube5 = randomizer.Next(11, 100);
+            Debug.WriteLine("5 = " + befCube5.ToString());
+            cubed1 = (int)Math.Pow(befCube1, 3);
+            cubed2 = (int)Math.Pow(befCube2, 3);
+            cubed3 = (int)Math.Pow(befCube3, 3);
+            cubed4 = (int)Math.Pow(befCube4, 3);
+            cubed5 = (int)Math.Pow(befCube5, 3);
+
+            cubedLabel1.Text = cubed1.ToString();
+            cubedLabel2.Text = cubed2.ToString();
+            cubedLabel3.Text = cubed3.ToString();
+            cubedLabel4.Text = cubed4.ToString();
+            cubedLabel5.Text = cubed5.ToString();
+
+            answer1.Value = 0;
+            answer2.Value = 0;
+            answer3.Value = 0;
+            answer4.Value = 0;
+            answer5.Value = 0;
+
+            answer1.Enabled = true;
+            answer2.Enabled = true;
+            answer3.Enabled = true;
+            answer4.Enabled = true;
+            answer5.Enabled = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+            timeLeft = 120 - level * 5;
+            fullTime = (int)timeLeft;
+            timer1.Start();
+            timeLeftVar.ForeColor = Color.SteelBlue;
+            timeLeftVar.Text = timeLeft.ToString();
+            levelVar.Text = (level + 1).ToString();
+        }
+
+        private bool CheckAnswers()
+        {
+            if (
+                (befCube1 == answer1.Value)
+                && (befCube2 == answer2.Value)
+                && (befCube3 == answer3.Value)
+                && (befCube4 == answer4.Value)
+                && (befCube5 == answer5.Value)
+                )
+            {
+                startButton.Enabled = true;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft <= fullTime * 0.6 && timeLeft > fullTime * 0.3)
+                timeLeftVar.ForeColor = Color.Orange;
+            else if (timeLeft <= fullTime * 0.3 && timeLeft > 0)
+                timeLeftVar.ForeColor = Color.Red;
+            if (CheckAnswers())
+            {
+                timer1.Stop();
+                if (level != 22)
+                {
+                    level++;
+                    startButton.Text = "Generate level " + (level + 1).ToString();
+                }
+                else
+                    startButton.Text = "Regenerate";
+                if (level != 22)
+                    MessageBox.Show("You have successfully completed level " + (level).ToString());
+                else if (firstTime == false)
+                {
+                    score++;
+                    scoreVal.Text = score.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("You have reached level 23. You will score points every time you finish in time. But once you fail, the game is over.");
+                    score = 0;
+                    scoreText.Visible = true;
+                    scoreVal.Visible = true;
+                    firstTime = false;
+                    scoreVal.Text = score.ToString();
+                }
+                startButton.Focus();
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft -= 0.1;
+                timeLeftVar.Text = ((int)timeLeft).ToString();
+            }
+            else
+            {
+                timer1.Stop();
+                startButton.Enabled = true;
+                timeLeftVar.Text = "0";
+                if (level > 0 && level < 2)
+                    level--;
+                else if (level > 2 && level < 22)
+                    level -= 2;
+                else if (level == 22)
+                {
+                    MessageBox.Show("You scored " + score.ToString());
+                    level = 0;
+                    afterFail = true;
+                };
+                if (level != 22)
+                    startButton.Text = "Generate level " + (level + 1).ToString();
+                else if (level == 22)
+                    startButton.Text = "Regenerate";
+                if (afterFail != true)
+                {
+                    MessageBox.Show("You have failed to finish in time");
+                }
+                else
+                    afterFail = false;
+                answer1.Value = befCube1;
+                answer2.Value = befCube2;
+                answer3.Value = befCube3;
+                answer4.Value = befCube4;
+                answer5.Value = befCube5;
+            }
+        }
+    }
+}
